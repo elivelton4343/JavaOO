3ddefeb449b2146f8b59bc170eb7cda308722f51
elivelton4343 <elivelton_gxp@hotmail.com>
2019-08-30 20:09:47 -0300

    add

---
+++ Suflair.java
@@ -0,0 +1,43 @@
+
+
+package app;
+
+/**
+ *
+ * @author aeliventon
+ */
+public class Suflair {
+    private String Nome;
+    private float valor;
+    private String Estoque;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getEstoque() {
+        return Estoque;
+    }
+
+    public void setEstoque(String Estoque) {
+        this.Estoque = Estoque;
+    }
+    
+    
+    
+    
+            
+    
+}
